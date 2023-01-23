8f3a9da34bf04ea9c916fb7eeac370cabeb1d2d4
Andrey-Rudenko <62091872+Andrey-Rudenko@users.noreply.github.com>
2023-01-24 04:40:16 +1000

    ProfileForm

--- webProgramming/react-app/src/components/ProfileForm.tsx
+++ webProgramming/react-app/src/components/ProfileForm.tsx
@@ -1,24 +1,66 @@
 ﻿import React from 'react';
+import Button from '@mui/material/Button';
 import AuthInput from '../obejcts/AuthInput';
 import authRepository from '../Repositories/AuthRepository';
+import { Autocomplete, Checkbox, FormControlLabel, FormGroup, TextField } from '@mui/material';
 
 const ProfileForm = () => {
 
     return (
         <div className="profile">
-                <h1>Авторизация</h1>
-                <div className="inputDiv">
-                    <h4>Логин</h4>
-                    <input></input> 
-                </div>
-                <div className="inputDiv">
-                    <h4>Пароль</h4>
-                    <input></input>
-                </div>
+            <form className="form">
+                <h1>Анкета</h1>
+                <TextField id="outlined-basic" label="Почта" variant="outlined" />
+                <Autocomplete className="autocomplete"
+                    multiple
+                    id="tags-outlined"
+                    options={tasklist}
+                    getOptionLabel={(option) => option.title}
+                   
+                    filterSelectedOptions
+                    renderInput={(params) => (
+                        <TextField
+                            {...params}
+                            variant="outlined"
+                            label="Знаю:"
+                            placeholder="Интересы"
+                        />
+                    )}
+                />
+                <Autocomplete className="autocomplete"
+                    multiple
+                    id="tags-outlined"
+                    options={tasklist}
+                    getOptionLabel={(option) => option.title}
+                  
+                    filterSelectedOptions
+                    renderInput={(params) => (
+                        <TextField
+                            {...params}
+                            variant="outlined"
+                            label="Хочу узнать"
+                            placeholder="Интересы"
+                        />
+                    )}
+                />
+            
+                <FormControlLabel control={<Checkbox defaultChecked />} label="Ищу собеседника" />
+                <FormControlLabel control={<Checkbox defaultChecked />} label="Хочу быть ментором" />
+                    <FormControlLabel control={<Checkbox defaultChecked />} label="Ищу ментора" />
 
-                <button>Войти</button> 
+                <button className="blackButton">Сохранить</button>
+        
+            </form>
         </div>
     );
 }
+
+const tasklist = [
+    { title: 'Говорить' },
+    { title: 'Петь' },
+    { title: 'Танцевать'},
+    { title: 'Программировать' },
+    { title: 'Зарабатывать' }
+];
 
 export default ProfileForm;
